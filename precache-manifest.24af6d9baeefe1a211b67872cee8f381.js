5daa3cef4a21b121ecce4342478886860fc692a6
Mikael Svensson <mikael.svensson@klarna.com>
2022-04-18 11:39:53 +0200

    deploy

---
+++ precache-manifest.24af6d9baeefe1a211b67872cee8f381.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3de828704bd141f3ec",
+    "url": "/css/about.662bfc66.css"
+  },
+  {
+    "revision": "e21f08171ac941ca5ce0",
+    "url": "/css/app.fba46803.css"
+  },
+  {
+    "revision": "1f2c0620f3c765c548fe",
+    "url": "/css/auth.85641d92.css"
+  },
+  {
+    "revision": "88f2216bb2c1199103a2",
+    "url": "/css/devicetest.a7b850f9.css"
+  },
+  {
+    "revision": "1c8be2d77b53e8227dc2",
+    "url": "/css/devicetest~map.30309ac8.css"
+  },
+  {
+    "revision": "32667ba0c0590805e77b",
+    "url": "/css/map.4a0df49d.css"
+  },
+  {
+    "revision": "e68811d102fccf99bca542fdb54cbc80",
+    "url": "/img/bullseye-round-purple-highres.e68811d1.png"
+  },
+  {
+    "revision": "b3d572be35bcd020766bc9f62bd7a74d",
+    "url": "/img/user-position-red-highres.b3d572be.png"
+  },
+  {
+    "revision": "0a944eaf80bd1c6638ae2cef96dfd814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b3de828704bd141f3ec",
+    "url": "/js/about.79c6ba8a.js"
+  },
+  {
+    "revision": "e21f08171ac941ca5ce0",
+    "url": "/js/app.595512bf.js"
+  },
+  {
+    "revision": "1f2c0620f3c765c548fe",
+    "url": "/js/auth.74cc6ec0.js"
+  },
+  {
+    "revision": "292a9aa2ae15ce3cc114",
+    "url": "/js/chunk-vendors.42e8696e.js"
+  },
+  {
+    "revision": "88f2216bb2c1199103a2",
+    "url": "/js/devicetest.b13edf6b.js"
+  },
+  {
+    "revision": "1c8be2d77b53e8227dc2",
+    "url": "/js/devicetest~map.71b75329.js"
+  },
+  {
+    "revision": "32667ba0c0590805e77b",
+    "url": "/js/map.db4d8bae.js"
+  },
+  {
+    "revision": "24a332b2d2495ff4ae59a62c4b240362",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d9fb89c389abdf64d96a32bdc80d193f",
+    "url": "/manifest/apple-touch-icon.png"
+  },
+  {
+    "revision": "a682cc30360618229f77f3a320e8e41f",
+    "url": "/manifest/favicon-16x16.png"
+  },
+  {
+    "revision": "2c40446995d7a8614a2f653b76c58df7",
+    "url": "/manifest/favicon-32x32.png"
+  },
+  {
+    "revision": "9d93cb3b02f5f6e0070cf0cfc6fb2c7b",
+    "url": "/manifest/mstile-150x150.png"
+  },
+  {
+    "revision": "62ff2946ea977463ee2b939674beabac",
+    "url": "/manifest/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
