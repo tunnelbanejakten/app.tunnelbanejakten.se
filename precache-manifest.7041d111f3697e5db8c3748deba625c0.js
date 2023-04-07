030e0528d89ff236b4f62bef471523ed779e40b2
Mikael Svensson <mikael.svensson@klarna.com>
2023-04-07 08:43:42 +0200

    deploy

---
+++ precache-manifest.7041d111f3697e5db8c3748deba625c0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec793a2ff21847ab33bd",
+    "url": "/css/app.18506adc.css"
+  },
+  {
+    "revision": "219a637baaa8e33e250c",
+    "url": "/css/chunk-vendors.f32eaac2.css"
+  },
+  {
+    "revision": "e68811d102fccf99bca542fdb54cbc80",
+    "url": "/img/bullseye-round-purple-highres.e68811d1.png"
+  },
+  {
+    "revision": "b3d572be35bcd020766bc9f62bd7a74d",
+    "url": "/img/user-position-red-highres.b3d572be.png"
+  },
+  {
+    "revision": "cab69afe9c4d899a5dfe1fcfd8e20711",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec793a2ff21847ab33bd",
+    "url": "/js/app.2cec14df.js"
+  },
+  {
+    "revision": "219a637baaa8e33e250c",
+    "url": "/js/chunk-vendors.257eda50.js"
+  },
+  {
+    "revision": "24a332b2d2495ff4ae59a62c4b240362",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d9fb89c389abdf64d96a32bdc80d193f",
+    "url": "/manifest/apple-touch-icon.png"
+  },
+  {
+    "revision": "a682cc30360618229f77f3a320e8e41f",
+    "url": "/manifest/favicon-16x16.png"
+  },
+  {
+    "revision": "2c40446995d7a8614a2f653b76c58df7",
+    "url": "/manifest/favicon-32x32.png"
+  },
+  {
+    "revision": "9d93cb3b02f5f6e0070cf0cfc6fb2c7b",
+    "url": "/manifest/mstile-150x150.png"
+  },
+  {
+    "revision": "62ff2946ea977463ee2b939674beabac",
+    "url": "/manifest/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
